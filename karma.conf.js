97f082adc4d3f29ec6e13be40b5b57f296e37851
Elliott Sprehn <esprehn@chromium.org>
2015-11-01 18:42:08 -0800

    Remove more stuff.

--- karma.conf.js
+++ karma.conf.js
@@ -1,26 +1,11 @@
 module.exports = function(config) {
   config.set({
-    // Up a level since we expect to be embedded inside bower_components/ in
-    // parallel to polymer itself.
-    basePath: "../",
+    basePath: ".",
     frameworks: ["mocha"],
     files: [
       // Test runner deps.
-      "fx-framework/node_modules/chai/chai.js",
-      "fx-framework/mocha.conf.js",
-
-      // Framework core deps.
-      "platform/platform.js",
-      "sugar/release/sugar.min.js",
-
-      // The actual tests.
-      "fx-framework/*-test.html",
-
-      // We need to be able to serve these files, but don't want to have them
-      // loaded automatically.
-      {pattern: "fx-framework/*.html", included: false},
-      {pattern: "platform/*", included: false},
-      {pattern: "polymer/*", included: false},
+      "/node_modules/chai/chai.js",
+      "/mocha.conf.js",
     ],
     reporters: ["progress"],
     port: 9876,
